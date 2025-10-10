a4b07f39a85a9ff674e809f7777faaa79dddb9e1
Smriti-Gupta2 <SmritiGupta0002@gmail.com>
2025-10-10 09:24:40 +0530

    Delete LABSHEET 02 AKSHAY directory

--- LABSHEET 02 AKSHAY/11_fixed_stack.c
+++
@@ -1,8 +0,0 @@
-
-#include <stdio.h>
-#define SIZE 5
-int stack[SIZE],top=-1;
-void push(int x){if(top==SIZE-1){printf("Overflow\n");}else{stack[++top]=x;}}
-void pop(){if(top==-1){printf("Underflow\n");}else{printf("Popped %d\n",stack[top--]);}}
-void display(){for(int i=0;i<=top;i++)printf("%d ",stack[i]);printf("\n");}
-int main(){push(10);push(20);display();pop();display();pop();pop();return 0;}
